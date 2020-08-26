2048a31b9c8bf39b712a10b662f17733bdc38e98
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-26 22:46:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,4 +19,8 @@ class Doctor
   def appointments
     Appointment.all.select { |appointment| appointment.doctor == self }
   end
+
+  def patients
+    appointments.map { |appointment| appointment.patient }
+  end
 end
